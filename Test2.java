4bcba2a6390b18fc79cf8db7d9737a5d9ae9fbbd
shajith <shajithlk@gmail.com>
2025-08-21 10:20:18 +0530

    Test2.java is created

---
+++ Test2.java
@@ -0,0 +1,6 @@
+public class Test2 {
+    public static void main(String[] args) {
+        // This line prints the text inside the quotation marks to the console
+        System.out.println("Hello, World!");
+    }
+}

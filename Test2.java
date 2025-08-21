d6085e979f0401d8d861a2daa0de4a407552d9cf
shajith <shajithlk@gmail.com>
2025-08-21 10:20:18 +0530

    newfle is created

---
+++ Test2.java
@@ -0,0 +1,12 @@
+public class Test2 {
+    public static void main(String[] args) {
+        // 1. Create a String variable to store a name.
+        String name = "Alex";
+
+        // 2. Print a greeting using the variable.
+        // The '+' sign joins the text strings together.
+        System.out.println("Hello mmmmnnnnnnooooo  test3, " + name + "!");
+	System.out.println("Hello ruhuna3, " + name + "!");
+
+    }
+}
